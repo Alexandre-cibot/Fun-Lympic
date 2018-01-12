309f658d9b1419774ca3de8417e1c9683a63dbf4
Alexandre Cibot <alexandre.cibot@gmail.com>
2018-01-12 11:28:50 +0100

    Remove Helloworld, add Stade page

---
+++ src/pages/Stadepage.vue
@@ -0,0 +1,56 @@
+<!--
+Ici, on testera l'implementation de phaser.
+Plus tard il y aura une page de selection de jeux en amont, mais ce n'est pas interessant pour le moment.
+Pour l'instant, nous testons ici.
+-->
+<template>
+<div class="wrapper">
+    <Navbar text="Entrainement" />
+  <div>
+  </div>
+  
+</div>
+</template>
+
+<script>
+import Card from '../components/Card';
+import Navbar from '../components/Navbar';
+import Classement from '../components/Classement';
+
+export default {
+  name: 'Training',
+  components: {
+    Card,
+    Navbar,
+    Classement,
+  },
+  data() {
+    return {
+      msg: 'test',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>  
+.wrapper {
+  background: #F5F5F5;
+  overflow: hidden;
+  height: 100vh;
+}
+h2, p {
+  font-family: 'Roboto';
+  font-weight: 100;
+  text-align: center;
+}
+h2 {
+  font-size: 1.2em;  
+  text-transform: uppercase;
+  padding-bottom: 1vh;
+}
+.score {
+  font-size: 2.2em;
+  margin-bottom: 1vh;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '../pages/HelloWorld.vue'
+
 import Homepage from '../pages/Homepage.vue'
 import Trainingpage from '../pages/Trainingpage.vue'
+import Stadepage from '../pages/Stadepage.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/hello',
-      name: 'Hello',
-      component: HelloWorld
-    },
     {
       path: '/',
       name: 'Homepage',
@@ -22,6 +18,11 @@ export default new Router({
       path: '/training',
       name: 'Training',
       component: Trainingpage
+    },
+    {
+      path: '/stade',
+      name: 'Stadepage',
+      component: Stadepage
     }
   ]
 })
