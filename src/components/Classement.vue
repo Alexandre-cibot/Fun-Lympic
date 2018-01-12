12f794e760b0edf6be22214ca94c9292d88d7eec
julien <julien.da-silva@hetic.net>
2018-01-12 10:48:58 +0100

    add components

---
+++ src/components/Classement.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="background">
+     <div class="container">
+    <div class="second">
+      <div class="circle">
+        <img src="../assets/enora.jpg" />
+      </div>
+      <div class="score">
+        <img src="../assets/stars_black.png" class="stars"/><p>{{ secondScore }}</p>
+      </div>
+    </div>
+    <div class="first">
+      <div class="circle circleCenter">
+        <img :src="imagePath" />
+        </div>
+      <div class="score">
+        <img src="../assets/stars_black.png" class="stars" /><p>{{ firstScore }}</p>
+      </div>
+    </div>
+    <div class="third">
+      <div class="circle">
+        <img src="../assets/hugo.jpg" />
+        </div>
+      <div class="score">
+        <img src="../assets/stars_black.png" class="stars"/><p>{{ thirdScore }}</p>
+      </div>      
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'classement',
+  props: {
+    firstScore: {
+      type: String,
+      required: true,
+    }, 
+    secondScore: {
+      type: String,
+      required: true,
+    },
+    thirdScore: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      msg: '',
+    };
+  },
+  computed: {
+    imagePath() {
+      return require('../assets/julien.jpg');
+    }
+  },
+  // computed: {
+  //   imagePath() {
+  //     switch (this.image) {
+  //       case 'hugo': 
+  //         return require('../assets/hugo.png');
+  //       break;
+  //       case 'julien': 
+  //         return require('../assets/julien.png');
+  //       break;
+  //       case 'enora': 
+  //         return require('../assets/enora.png');
+  //       break;
+  //       default: 
+  //         console.log('No default found');
+  //       break;
+  //     }
+  //   }
+  // }
+};
+</script>
+
+<style scoped>
+.circle, .circleCenter{
+  min-height: 0;
+  overflow: visible;
+  height: 100%;
+  width:100%;
+  text-align: center;
+  margin-bottom: 10px;
+}
+.circleCenter{
+  height: 100%;
+  overflow: visivle;
+  transform: scale(1.3);
+}
+
+.circleCenter img {
+  position: absolute;
+  left: 0;
+  top: -15%;
+  left: 50%;
+  transform: translateX(-50%);
+}
+.score{
+  width: 55%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+}
+.stars {
+  float: left; 
+  display: inline-block;
+}
+p {
+  display: inline-block;
+}
+.first, .second, .third {
+  display: flex;
+  flex-direction: column;
+  /* border: 1px solid red; */
+  align-items: center;
+  justify-content: space-evenly;
+  font-family: Roboto;
+  width: 100%;
+}
+.background {
+  position: absolute;
+  bottom: 0;
+  width: 84vw;
+  height: 10vh;
+  border-radius: 20px 20px 0 0;
+  background: white;
+  transform: translateX(-50%);
+  left: 50%;
+}
+
+.container{
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  bottom: 20%;
+  width: 100%;
+  height: 150%;
+  border-radius: 20px 20px 0 0;
+  transform: translateX(-50%);
+  left: 50%;
+  justify-content: space-evenly;
+  margin-bottom: 0px;
+}
+.circle img, .circleCenter img{
+  width:auto;
+  height: calc(100% - 8px);
+  /* border: 1px solid green; */
+  border-radius: 50%;
+  border: 8px solid white;
+}
+</style>
---
+++ src/components/basicButton.vue
@@ -0,0 +1,109 @@
+<!--
+Exemple pour apeller ce composant : 
+<BasicButton title="entrainement" btnColor="btnBlue" @doAction="handleClick" />
+-->
+<template>
+  <button class="basicButton btn-dFlex" :class="btnColor" @click="handleClick" v-bind:style="{ 'justify-content': image ? 'space-between' : 'center' }">
+    <img v-if="image" :src="imagePath" alt="" class="btn-fLeft btn-icone">
+    <p class="btn-fRight btn-title">
+      {{title}}
+    </p>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'basicButton',
+  props: {
+    btnColor: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    handleClick() {
+      this.$emit('doAction');
+    }
+  },
+  computed: {
+    imagePath() {
+      switch(this.image) {
+        case 'megaphone':
+          return require('../assets/loud.png');
+        break;
+        case 'olympique':
+          return require('../assets/olympique.png');
+        break;
+        case 'facebook':
+          return require('../assets/facebook.png');
+        break;
+        case 'run':
+          return require('../assets/run.png');
+        break;
+        default:
+          console.log('no image default found.');
+        break;
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>  
+  .basicButton p {
+    font-family: roboto, sans-serif;
+    font-weight: lighter;
+  }
+  .btnBlue {
+    border: none;
+    border-radius: 10px;
+    box-shadow: 0px 8px 0px 0px #2E00AB;
+    background-color: #595BEE;
+    padding: 12px;
+  }
+  .btnYellow {
+    border: none;
+    border-radius: 20px;
+    box-shadow: 0px 8px 0px 0px #C09033;
+    background-color: #F7BE51;
+    padding: 12px;
+  }
+  .btn-fLeft {
+    float:left;
+  }
+  .btn-Right {
+    float:right;
+  }
+  .btn-dFlex {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    flex-direction: row;
+    align-items: center;
+    padding: 15px 30px;
+  }
+  .btn-title {
+    font-size: 20px;
+    text-transform: uppercase;
+    color: white;
+  }
+  .btn-icone {
+    width: auto;
+    height: 30px;
+    padding-right:10px;
+  }
+</style>
