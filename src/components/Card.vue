899e660057652d711c96dc6e07cf8e1a96953c54
julien <julien.da-silva@hetic.net>
2018-01-12 10:48:58 +0100

    add components

---
+++ src/components/Card.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="card">
+    <h3>{{text}}</h3>
+    <img :src="imagePath" />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'card',
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      msg: '',
+    };
+  },
+  computed: {
+    imagePath() {
+      switch (this.image) {
+        case 'athletisme': 
+          return require('../assets/escrime.png');
+        break;
+        default: 
+          console.log('No default found');
+        break;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  position: relative;
+  padding-top: 50px;
+  background: #F7BE51;
+  border-radius: 20px;
+  height: 42vh;
+  z-index: 20;
+  width: 75%;
+  margin: auto;
+  box-shadow: 0px 8px 0px 0px rgba(192,144,51,1);
+}
+.card:before {
+  /* content: "";
+  transform: rotate(-52deg) translateX(-20px) translateY(100px);
+  z-index:0;
+  position: absolute;
+  height: 100%;
+  width: 162%;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.04); */
+}
+img{
+  display: block;
+  position: relative;
+  width: 129px;
+  margin: auto;
+  margin-top: 30px;
+}
+h3{
+  padding: 15px;
+  font-size: 1.5em;
+  font-family: 'Helvetica';
+  font-weight: 100;
+  text-align: center;
+  background: #DCA233;
+  color: white;
+  text-transform: uppercase;
+}
+</style>
