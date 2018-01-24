b3fa856c94b16b825a0e8411625e746168736bf9
Jérémy Bourel <calaruni@hotmail.com>
2018-01-24 22:07:00 +0100

    add classement for friends of current user in a game

---
+++ src/components/ClassementFriends.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="listClassement">
+      <img :src="picture" alt="">
+      <h2>{{firstname}}</h2>
+      <h3>{{points}}</h3>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ClassementFriends',
+  props: {
+    firstname: {
+      type: String,
+      required: true,
+    }, 
+    picture: {
+      type: String,
+      required: true,
+    },
+    points: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.listClassement {
+  display: flex;
+  background: white;
+}
+
+</style>
