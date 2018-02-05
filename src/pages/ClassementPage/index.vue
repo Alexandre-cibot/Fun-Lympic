62244b08bca6327b2761dcdd53dc7e64cc8587c6
Jérémy Bourel <calaruni@hotmail.com>
2018-02-05 22:03:57 +0100

    change the classement when the user clicks on button

--- src/pages/ClassementPage/index.vue
+++ src/pages/ClassementPage/index.vue
@@ -4,12 +4,12 @@
     <div class="wrapper">
       
       <button class="button_main">
-        <button class="left_arr">
+        <button class="left_arr" @click="changeSport">
           <img src="@/assets/left_arr.svg" alt="">
         </button>
         <img src="@/assets/win_white.svg" alt="" class="win">
-        <p>Athlétisme</p>
-        <button class="right_arr">
+        <p>{{sport}}</p>
+        <button class="right_arr" @click="changeSport">
           <img src="@/assets/right_arr.svg" alt="">
         </button>
       </button>
@@ -24,9 +24,9 @@
         </div>
       </div>
       <div class="button_bottom">
-        <ClassementButton image="facebook" btnColor="yellow" />
-        <ClassementButton image="flag" btnColor="blue" />
-        <ClassementButton image="planet" btnColor="blue" />
+        <ClassementButton image="facebook"  @click.native="facebook" :class="currentState == 'facebook' ? 'btnYellow' : 'btnBlue'"/>
+        <ClassementButton image="flag" @click.native="flag" :class="currentState == 'flag' ? 'btnYellow' : 'btnBlue'"/>
+        <ClassementButton image="planet" @click.native="planet" :class="currentState == 'planet' ? 'btnYellow' : 'btnBlue'"/>
       </div>
     </div>
   </div>  
@@ -36,7 +36,6 @@
 import Navbar from '@/components/Navbar';
 import ClassementFriends from '@/components/ClassementFriends';
 import ClassementButton from '@/components/ClassementButton';
-
 export default {
   name: 'Classement',
   components: {
@@ -44,21 +43,13 @@ export default {
     ClassementButton,
     Navbar
   },
-  computed: {
-    friendsArray: function() {
-      function compare(a, b) {
-        if (a.points > b.points)
-          return -1;
-        if (a.points < b.points)
-          return 1;
-        return 0;
-      }
-
-      return this.friends.sort(compare);
-    }
-  },
   data(){
     return{
+      currentState: 'facebook',
+      facebookClassement : true,
+      flagClassement : false,
+      planetClassement : false,
+      sport : "Athlétisme",
       friends: [
         {
           firstname: "Enora",
@@ -114,10 +105,147 @@ export default {
           picture: require('@/assets/julien.jpg'),
           flag: require('@/assets/flag/france.svg'),
         },
+      ],
+      friendsFlag: [
+        {
+          firstname: "Julien",
+          points: 290,
+          picture: require('@/assets/julien.jpg'),
+          flag: require('@/assets/flag/france.svg'),
+        },
+        {
+          firstname: "Julien",
+          points: 220,
+          picture: require('@/assets/julien.jpg'),
+          flag: require('@/assets/flag/france.svg'),
+        },
+        {
+          firstname: "Enora",
+          points: 200,
+          picture: require('@/assets/enora.jpg'),
+          flag: require('@/assets/flag/france.svg'),
+        },
+        {
+          firstname: "Julien",
+          points: 190,
+          picture: require('@/assets/julien.jpg'),
+          flag: require('@/assets/flag/france.svg'),
+        },
+                {
+          firstname: "Enora",
+          points: 170,
+          picture: require('@/assets/enora.jpg'),
+          flag: require('@/assets/flag/france.svg'),
+        },
+        {
+          firstname: "Julien",
+          points: 150,
+          picture: require('@/assets/julien.jpg'),
+          flag: require('@/assets/flag/france.svg'),
+        },
+        {
+          firstname: "Julien",
+          points: 140,
+          picture: require('@/assets/julien.jpg'),
+          flag: require('@/assets/flag/france.svg'),
+        },
+                {
+          firstname: "Enora",
+          points: 130,
+          picture: require('@/assets/enora.jpg'),
+          flag: require('@/assets/flag/france.svg'),
+        },
+        {
+          firstname: "Julien",
+          points: 110,
+          picture: require('@/assets/julien.jpg'),
+          flag: require('@/assets/flag/france.svg'),
+        },
+      ],
+      friendsPlanet: [
+        {
+          firstname: "Julien",
+          points: 490,
+          picture: require('@/assets/julien.jpg'),
+          flag: require('@/assets/flag/france.svg'),
+        },
+        {
+          firstname: "Julien",
+          points: 380,
+          picture: require('@/assets/julien.jpg'),
+          flag: require('@/assets/flag/france.svg'),
+        },
+        {
+          firstname: "Enora",
+          points: 360,
+          picture: require('@/assets/enora.jpg'),
+          flag: require('@/assets/flag/france.svg'),
+        },
+        {
+          firstname: "Julien",
+          points: 330,
+          picture: require('@/assets/julien.jpg'),
+          flag: require('@/assets/flag/france.svg'),
+        },
+                {
+          firstname: "Enora",
+          points: 270,
+          picture: require('@/assets/enora.jpg'),
+          flag: require('@/assets/flag/france.svg'),
+        },
+        {
+          firstname: "Julien",
+          points: 250,
+          picture: require('@/assets/julien.jpg'),
+          flag: require('@/assets/flag/france.svg'),
+        },
+        {
+          firstname: "Julien",
+          points: 190,
+          picture: require('@/assets/julien.jpg'),
+          flag: require('@/assets/flag/france.svg'),
+        },
+                {
+          firstname: "Enora",
+          points: 140,
+          picture: require('@/assets/enora.jpg'),
+          flag: require('@/assets/flag/france.svg'),
+        },
+        {
+          firstname: "Julien",
+          points: 110,
+          picture: require('@/assets/julien.jpg'),
+          flag: require('@/assets/flag/france.svg'),
+        },
       ]
     }
   },
   methods: {
+    changeSport(){
+      if(this.sport == "Athlétisme"){
+        this.sport = "Natation";
+      }else {
+        this.sport = "Athlétisme";
+      }
+    },
+    facebook(){
+      this.currentState = `facebook`;
+      this.flagClassement = false;
+      this.facebookClassement = true;
+      this.planetClassement = false;
+    },
+    flag(){
+      this.currentState = `flag`;
+      this.flagClassement = true;
+      this.facebookClassement = false;
+      this.planetClassement = false;
+    },
+    planet(){
+      this.currentState = `planet`;
+      this.flagClassement = false;
+      this.facebookClassement = false;
+      this.planetClassement = true;
+    },
     goBack(){
       this.$router.push('/');
       // let previousState = this.history[this.history.length-1];
@@ -128,7 +256,26 @@ export default {
       //   this.history.pop(); // Remove the last one
       // }
     }
-  }
+  },
+  computed: {
+    friendsArray (array) {
+      function compare(a, b) {
+        if (a.points > b.points)
+          return -1;
+        if (a.points < b.points)
+          return 1;
+        return 0;
+      }
+      if (this.currentState == 'facebook'){
+        this.array = this.friends;
+      }else if (this.currentState == 'flag') {
+        this.array = this.friendsFlag;
+      }else {
+        this.array = this.friendsPlanet;
+      }
+      return this.array.sort(compare);
+    }
+  },
 }
 </script>
 
@@ -136,6 +283,14 @@ export default {
 .wrapper-page {
   height: 100vh;
   width: 100%;
+}
+.btnYellow {
+    background: #F9C947;
+    box-shadow: 0px 4px 0px 0px #D8AD41;
+}
+.btnBlue{
+  background: #57C9D7;
+  box-shadow: 0px 4px 0px 0px #10B2C0;
 }
 .wrapper {
   display: flex;
