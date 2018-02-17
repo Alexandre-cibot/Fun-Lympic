2112e274ae45d0fc38dbdc9dd595229c7ee2ccc2
Jérémy Bourel <calaruni@hotmail.com>
2018-02-17 18:15:35 +0100

    add page shopping and character

--- src/pages/ShoppingPage/index.vue
+++ src/pages/ShoppingPage/index.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="wrapper-page bg_purple_light">
-    <Navbar text="Marcelin" :shopping="true" :money="400" secondBtn="shopping" @goBack="goBack"/>
+    <Navbar text="Marcelin" :shopping="true" :money="400" secondBtn="none" @goBack="goBack"/>
     <div class="wrapper">
-      <div v-for="info in infos" :key="info.index" >
-        <ShoppingButton :price="info.price" :picture="info.picture" :promo="info.promo"/>
+      <button class="btnYellow"></button>
+      <div class="wrap_price">
+        <div v-for="info in infos" :key="info.index" class="price">
+          <ShoppingButton :price="info.price" :picture="info.picture" :promo="info.promo" :coin="info.coin" :percentage="info.percentage"/>
+        </div>
       </div>
     </div>
   </div>
@@ -11,21 +14,45 @@
 
 <script>
 import Navbar from '@/components/Navbar';
+import BasicButton from '@/components/BasicButton';
 import ShoppingButton from './components/ShoppingButton';
 
 export default {
   name: "Shopping",
   components: {
     Navbar,
-    ShoppingButton
+    ShoppingButton,
+    BasicButton
   },
   data(){
     return{
       infos:[
         {
-          price: 20.00,
-          picture: require('@/assets/jo_coin.png'),
+          price: "1,10",
+          coin: 1500,
+          picture: require('@/assets/one_jo_coin.png'),
           promo: false
+        },
+        {
+          price: "3,00",
+          coin: 5000,
+          picture: require('@/assets/two_jo_coin.png'),
+          promo: true,
+          percentage: 15 
+        },
+        {
+          price: "7,50",
+          coin: 13000,
+          picture: require('@/assets/three_jo_coin.png'),
+          promo: true,
+          percentage: 30
+        },
+        {
+          price: "20,00",
+          coin: 50000,
+          picture: require('@/assets/four_jo_coin.png'),
+          promo: true,
+          percentage: 65
         }
       ]
     }
@@ -35,7 +62,6 @@ export default {
       window.history.go(-1)
     }
   }
-
 }
 </script>
 <style scoped>
@@ -43,11 +69,30 @@ export default {
   height: 100%;
   overflow-y: hidden;
 }
+.btnYellow {
+  box-shadow: 0px 6px 0px 0px #F1B946;
+  background-color: #FFD360;
+  font-size: 1.7em;
+  width: 100%;
+  min-height: 8vh;
+}
+.wrap_price{
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+.price{
+  width: 47%;
+  margin-top: 3vh;
+}
 .wrapper {
   display:flex;
   flex-direction: column;
   justify-content: center;
   overflow-y: scroll;
+  padding: 0 20px;
   height: calc(100% - 13vh);
   padding-top: 3vh;
 }
