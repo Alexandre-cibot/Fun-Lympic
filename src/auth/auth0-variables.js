4ac643473a678a633f3e2d680b44130a88802d2c
Alexandre Cibot <alexandre.cibot@gmail.com>
2018-02-19 00:46:38 +0100

    Changin redirection URL

--- src/auth/auth0-variables.js
+++ src/auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   clientId: 'x3uud7Le3fyXh2XTHosUxPfQ1jlStn9A',
   domain: 'jo-games.eu.auth0.com',
-  callbackUrl: 'http://localhost:3000/',
+  callbackUrl: 'https://jo-games.netlify.com/',
   apiUrl: 'API_IDENTIFIER'
 }
