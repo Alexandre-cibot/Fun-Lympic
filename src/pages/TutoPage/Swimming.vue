5c7e2512e907b59c95cffc0053585926c219edb8
Jérémy Bourel <calaruni@hotmail.com>
2018-02-18 21:30:19 +0100

    add super combo in swimming game

---
+++ src/pages/TutoPage/Swimming.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="tuto" @click="disappear"></div>  
+</template>
+<script>
+export default {
+  name:"AthleticsTuto",
+  methods:{
+    disappear(){
+      this.$router.push('/swimming');
+    }
+  }
+}
+</script>
+<style>
+.container{
+  position: relative;
+  height: 100vh;
+  width: 100%;
+  background: black;
+}
+#tuto{
+  position: absolute;
+  background: url('../../../assets/images/Tuto_swim.jpg');
+  background-size: cover;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  /* z-index: 99; */
+}
+</style>
+
+
