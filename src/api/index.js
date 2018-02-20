e7dbb7656754e39b742175d0dae46f8e456282b1
Alexandre Cibot <alexandre.cibot@gmail.com>
2018-02-20 23:48:36 +0100

    Lien vers API

---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+const URL = isProduction ? 'http://alexandrecibot.com:8080' : 'http://localhost:8080'
+
+export default {
+  postUser (dataFromFacebook, callback) {
+    axios.post(`${URL}/users`, dataFromFacebook).then(callback)
+  }
+}
