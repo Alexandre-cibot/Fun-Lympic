87f5408865e1613f1603dd72c241d2d313fafabd
Alexandre Cibot <alexandre.cibot@gmail.com>
2018-02-21 10:38:30 +0100

    HTTPS Le retour !

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-// const URL = isProduction ? 'http://alexandrecibot.com:8080' : 'http://localhost:8080'
-const URL = 'http://alexandrecibot.com:8080'
+// const URL = isProduction ? 'https://gentle-cove-60320.herokuapp.com' : 'http://localhost:8080'
+const URL = 'https://gentle-cove-60320.herokuapp.com'
 export default {
   postUser (dataFromFacebook, callback) {
     axios.post(`${URL}/users`, dataFromFacebook).then(callback)
