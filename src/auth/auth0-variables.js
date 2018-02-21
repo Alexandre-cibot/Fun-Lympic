9e9f6c15b445a6c495acf0ac2f24f0c8ef03e67a
Alexandre Cibot <alexandre.cibot@gmail.com>
2018-02-21 10:38:30 +0100

    HTTPS Le retour !

--- src/auth/auth0-variables.js
+++ src/auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   clientId: 'x3uud7Le3fyXh2XTHosUxPfQ1jlStn9A',
   domain: 'jo-games.eu.auth0.com',
-  callbackUrl: isProduction ? 'http://jo-games.netlify.com/' : 'http://localhost:3000/',
+  callbackUrl: isProduction ? 'https://jo-games.netlify.com/' : 'http://localhost:3000/',
   apiUrl: 'API_IDENTIFIER'
 }
