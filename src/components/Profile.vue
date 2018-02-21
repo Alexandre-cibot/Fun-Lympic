d52c7d1a059a28b6f133810c63678f9fcd1e76c4
Alexandre Cibot <alexandre.cibot@gmail.com>
2018-02-21 11:42:05 +0100

    Animation 'Profile' binding defeats&victories

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -48,6 +48,36 @@ export default {
 </script>
 
 <style scoped>
+
+@-webkit-keyframes rotating /* Safari and Chrome */ {
+  from {
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  to {
+    -webkit-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes rotating {
+  from {
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  to {
+    -ms-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -webkit-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+
 .wrapper_profile{
   display: flex;
   flex-direction: column; 
@@ -87,6 +117,11 @@ export default {
 }
 .circle {
   height: 20vh;
+  -webkit-animation: rotating 60s linear infinite;
+  -moz-animation: rotating 60s linear infinite;
+  -ms-animation: rotating 60s linear infinite;
+  -o-animation: rotating 60s linear infinite;
+  animation: rotating 60s linear infinite;
 }
 h4{
   margin-top: 1vh;
