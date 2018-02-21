31754cd6b8b5ed6ea7e9b1fb37f2494323e76b68
Alexandre Cibot <alexandre.cibot@gmail.com>
2018-02-21 01:19:16 +0100

    API prod even in dev

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-const URL = isProduction ? 'http://alexandrecibot.com:8080' : 'http://localhost:8080'
-
+// const URL = isProduction ? 'http://alexandrecibot.com:8080' : 'http://localhost:8080'
+const URL = 'http://alexandrecibot.com:8080'
 export default {
   postUser (dataFromFacebook, callback) {
     axios.post(`${URL}/users`, dataFromFacebook).then(callback)
