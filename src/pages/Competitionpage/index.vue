79331df2a9337edb553454598110dc50423da816
Jérémy Bourel <calaruni@hotmail.com>
2018-02-22 00:45:00 +0100

    add page score and edit font

--- src/pages/Competitionpage/index.vue
+++ src/pages/Competitionpage/index.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="wrapper-page bg_purple_light">
-    <Navbar text="Défie tes amis" secondBtn="podium" @goBack="goBack"/>
+    <Navbar v-if="currentState != 'winGame' && currentState != 'loseGame' && currentState != 'playWithFriend'" text="Défie tes amis" secondBtn="podium" @goBack="goBack"/>
     <div class="wrapper">
       <ConnexionFB v-if="!authenticated" :authenticated="authenticated" :auth="auth" />
       <div v-else style="height:100%">
-        <CompetitionDashboard v-if="currentState === 'competitionDashboard' || currentState === 'chooseFlag'" :profile="profile" @chooseFlag="chooseFlag" @playWithFriend="playWithFriend"/>
-        <ChallengeGame v-if="currentState === 'playWithFriend'" />
+        <CompetitionDashboard v-if="currentState === 'competitionDashboard' || currentState === 'chooseFlag'" :profile="profile" @chooseFlag="chooseFlag" @playWithFriend="playWithFriend" @winGame="winGame" @loseGame="loseGame"/>
+        <ChallengeGame v-if="currentState === 'playWithFriend'" @pushRoute="pushRoute"/>
+        <WinGame v-if="currentState === 'winGame'" @pushRoute="pushRoute"/>
+        <LoseGame  v-if="currentState === 'loseGame'" @pushRoute="pushRoute"/>
         <Nation v-if="currentState === 'chooseFlag'"  @closeModal="closeModal" @chooseFriends="chooseFriends" />
         <Friends v-if="currentState === 'chooseFriends'" @friend="friend"/>
         <ChallengeFriend v-if="currentState === 'friend'" @before="before" />
-        <BeforeGame v-if="currentState === 'before'" />        
+        <BeforeGame v-if="currentState === 'before'" />
         <!-- <ClassementGame /> -->
       </div>
     </div>
@@ -30,6 +32,8 @@ import ChallengeFriend from './components/ChallengeFriend.vue';
 import Friends from './components/Friends.vue';
 import Nation from './components/Nation';
 import ChallengeGame from './components/ChallengeGame';
+import WinGame from './components/WinGame';
+import LoseGame from './components/LoseGame';
 
 export default {
   name: 'Stade',
@@ -38,6 +42,8 @@ export default {
     Navbar,
     Friends,
     BasicButton,
+    WinGame,
+    LoseGame,
     ConnexionFB,
     ChallengeGame,
     ChallengeFriend,
@@ -70,6 +76,10 @@ export default {
       this.updateHistory(this.currentState);
       this.currentState = "competitionDashboard";
     },
+    pushRoute(){
+      this.updateHistory(this.currentState);
+      this.currentState = "competitionDashboard";
+    },
     // connection() {
     //   this.connexion = !this.connexion;
     // },
@@ -84,6 +94,14 @@ export default {
     chooseFlag() {
       this.updateHistory(this.currentState);
       this.currentState = "chooseFlag"
+    },
+    loseGame(){
+      this.updateHistory(this.currentState);
+      this.currentState = "loseGame";
+    },
+    winGame(){
+      this.updateHistory(this.currentState);
+      this.currentState = "winGame";
     },
     friend() {
       this.updateHistory(this.currentState);
