23337c06079efefef410bb4358f68d6742f221a9
julien <julien.da-silva@hetic.net>
2018-02-22 12:00:46 +0100

    use new heroku api URL

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 // const URL = isProduction ? 'https://gentle-cove-60320.herokuapp.com' : 'http://localhost:8080'
-const URL = 'https://gentle-cove-60320.herokuapp.com'
+// const URL = 'https://gentle-cove-60320.herokuapp.com'
+// const URL = 'http://localhost:8080'
+const URL = 'https://morning-inlet-27716.herokuapp.com'
 export default {
   postUser (dataFromFacebook, callback) {
     axios.post(`${URL}/users`, dataFromFacebook).then(callback)
