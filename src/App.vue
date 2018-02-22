276606336adafe123d3a71d4f9f88495bab91084
Veronique <henka-nk@hotmail.com>
2018-02-22 22:55:45 +0100

    sound

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
     <router-view
-    :auth="auth" 
+    :auth="auth"
     :authenticated="authenticated">
     </router-view>
+
+    <Sound file="ambiance" />
   </div>
 </template>
 
@@ -14,19 +16,25 @@ if (location.protocol != 'https:' && !location.origin.includes('localhost') && !
   location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
 }
 
-import AuthService from './auth/AuthService'
+import AuthService from './auth/AuthService';
+import Sound from '@/components/Sound';
+import store from '@/store';
+
 const auth = new AuthService()
 const { login, logout, authenticated, authNotifier } = auth
 
 export default {
   name: 'app',
+  components: {
+    Sound
+  },
   data () {
     authNotifier.on('authChange', authState => {
       this.authenticated = authState.authenticated
     })
     return {
       auth,
-      authenticated
+      authenticated,
     }
   },
   mounted() {
@@ -34,6 +42,16 @@ export default {
     // TODO: use location.hash rather than location.href
     if (window.location.href.includes('/#/access_token')) {
       this.auth.handleAuthentication();
+    }
+
+    // Sound muted localstorage
+    var sound = JSON.parse(localStorage.getItem('sound'));
+    if( sound && sound.muteBy == 'player') {
+      store.commit('toggleSoundMute', sound)
+    } else if(sound && sound.isMuted && sound.muteBy == 'game') {
+      var newSound = {'isMuted' : false, 'muteBy' : 'none'};
+      store.commit('toggleSoundMute', newSound);
+      localStorage.setItem('sound', JSON.stringify(newSound))
     }
   },
   methods: {
